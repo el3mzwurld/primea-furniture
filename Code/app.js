49f468746c23c3701df460810bedf67892cd5c71
elemz <elemisamuel77@gmail.com>
2025-08-26 17:18:55 +0100

    First commit for Primea Furniture

---
+++ Code/app.js
@@ -0,0 +1,12 @@
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const display = document.querySelector(".display");
+const slides = document.querySelectorAll(".slide");
+
+//Index
+let index = 0;
+
+function showSlide(i) {
+  if (index > 0) {
+  }
+}
