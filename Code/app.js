50f5721d5f559cb476aa3b052bfe0c9f0b41b65d
elemz <elemisamuel77@gmail.com>
2025-08-28 13:39:02 +0100

    ...

--- Code/app.js
+++ Code/app.js
@@ -1,12 +1,34 @@
-const left = document.querySelector("#left");
-const right = document.querySelector("#right");
-const display = document.querySelector(".display");
-const slides = document.querySelectorAll(".slide");
+const slides = document.querySelectorAll(".img");
 
-//Index
-let index = 0;
+let slideIndex = 0;
+initSlide();
 
-function showSlide(i) {
-  if (index > 0) {
-  }
+function initSlide() {
+  slides[slideIndex].classList.add("showslide");
 }
+function showslide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides.forEach((slide) => {
+    slide.classList.remove("showslide");
+  });
+  slides[slideIndex].classList.add("showslide");
+}
+
+function nextSlide() {
+  slideIndex += 1;
+  showslide();
+}
+function prevSlide() {
+  slideIndex -= 1;
+  showslide();
+}
+
+const next = document.querySelector("#right");
+const prev = document.querySelector("#left");
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
